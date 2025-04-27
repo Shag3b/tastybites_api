35557af0b1f25c93fc2e773e463b7edcad7c4143
Shag3b <180705089+Shag3b@users.noreply.github.com>
2025-04-27 20:20:03 +0300

    register to host

--- tastybites_api/settings.py
+++ tastybites_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c=4_xse1rok7c*d^^zf=8%-0-)g4!j!mc6+zjh76o3a_fi)!ft'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['eldeeb.pythonanywhere.com']
+ALLOWED_HOSTS = ['eldeeb.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
