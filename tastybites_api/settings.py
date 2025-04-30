9b45b5c7180c4be68572991a9c7e682d7d138f30
Shag3b <180705089+Shag3b@users.noreply.github.com>
2025-04-30 04:37:58 +0300

    swagger&redoc-endpoint_add

--- tastybites_api/settings.py
+++ tastybites_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'menu',
     'orders',
+     'drf_yasg',  # Add turlhis line
 ]
 
 MIDDLEWARE = [
--- tastybites_api/urls.py
+++ tastybites_api/urls.py
@@ -18,12 +18,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+#url swaager
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TastyBites API",
+        default_version='v1',
+    ),
+    public=True,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/menu/', include('menu.urls')),
     path('api/orders/', include('orders.urls')),
+      path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),#swager
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
 
 # Add this at the end for media files
